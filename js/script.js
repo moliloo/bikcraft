0a02b0b3da74a3d3da84550f747baa39a36bffb3
moliloo <jhjmbrito@gmail.com>
2024-03-06 15:42:48 -0300

    added toggle in bike galley and optmized script.js

--- js/script.js
+++ js/script.js
@@ -1,39 +1,43 @@
 const links = document.querySelectorAll('.header-menu a')
 
-function activateLink(link) {
+links.forEach((link) => {
 	const url = location.href
 	const href = link.href
 	if (url.includes(href)) {
 		link.classList.add('active')
 	}
-}
-
-links.forEach(activateLink)
+})
 
 const parametros = new URLSearchParams(location.search)
 
-function activateProduct(parameter) {
+parametros.forEach((parameter) => {
 	const element = document.getElementById(parameter)
 	if (element) {
 		element.checked = true
 	}
-}
-
-parametros.forEach(activateProduct)
+})
 
 const perguntas = document.querySelectorAll('.perguntas button')
 
-function clickFaq(event) {
-	const pergunta = event.currentTarget
-	const controls = pergunta.getAttribute('aria-controls')
-	const resposta = document.getElementById(controls)
-	resposta.classList.toggle('ativo')
-	const togleAria = resposta.classList.contains('ativo')
-	pergunta.setAttribute('aria-expanded', togleAria)
-}
+perguntas.forEach((pergunta) => {
+	pergunta.addEventListener('click', (event) => {
+		const pergunta = event.currentTarget
+		const controls = pergunta.getAttribute('aria-controls')
+		const resposta = document.getElementById(controls)
+		resposta.classList.toggle('ativo')
+		const togleAria = resposta.classList.contains('ativo')
+		pergunta.setAttribute('aria-expanded', togleAria)
+	})
+})
 
-function clickExpand(pergunta) {
-	pergunta.addEventListener('click', clickFaq)
-}
+const galeria = document.querySelectorAll('.bicicleta-imagens img')
+const galeriaContainer = document.querySelector('.bicicleta-imagens')
 
-perguntas.forEach(clickExpand)
+galeria.forEach((img) => {
+	img.addEventListener('click', (event) => {
+		const img = event.currentTarget
+		if (matchMedia('(min-width: 1000px)').matches) {
+			galeriaContainer.prepend(img)
+		}
+	})
+})
